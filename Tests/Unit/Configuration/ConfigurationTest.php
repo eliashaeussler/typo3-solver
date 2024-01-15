9c2f819dbc22539a5143c41117a9f4817f46b764
Elias Häußler <elias@haeussler.dev>
2024-01-15 20:08:10 +0100

    [TASK] Run PHP-CS-Fixer

--- Tests/Unit/Configuration/ConfigurationTest.php
+++ Tests/Unit/Configuration/ConfigurationTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the TYPO3 CMS extension "solver".
  *
- * Copyright (C) 2023 Elias Häußler <elias@haeussler.dev>
+ * Copyright (C) 2024 Elias Häußler <elias@haeussler.dev>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
