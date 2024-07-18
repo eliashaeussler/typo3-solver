11000cd7089f635ca47d8c86d7627b1fd25c9870
Elias Häußler <elias@haeussler.dev>
2024-07-18 21:22:44 +0200

    [FEATURE] Add support for TYPO3 v13.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'elias@haeussler.dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-13.1.99',
+            'typo3' => '11.5.0-13.2.99',
             'php' => '8.1.0-8.3.99',
         ],
     ],
